1d4bdcd5752057456d33680cc7589ab521d7a24b
darshan <darshanreddykoneru@gmail.com>
2024-11-09 21:02:04 +0530

    edit.html

---
+++ script.js
@@ -0,0 +1,93 @@
+let btn=document.querySelector("#btn")
+let content=document.querySelector("#content")
+let voice=document.querySelector("#voice")
+
+function speak(text){
+    let text_speak=new SpeechSynthesisUtterance(text)
+    text_speak.rate=1
+    text_speak.pitch=1
+    text_speak.volume=1
+    window.speechSynthesis.speak(text_speak)
+}
+
+function wishMe(){
+    let day=new Date()
+    let hours=day.getHours()
+    if(hours>=0 && hours<12){
+        speak("Good Morning ")
+    }
+    else if(hours>=12 && hours <16){
+        speak("Good afternoon ")
+    }else{
+        speak("Good Evening")
+    }
+}
+let speechRecognition= window.SpeechRecognition || window.webkitSpeechRecognition 
+let recognition =new speechRecognition()
+recognition.onresult=(event)=>{
+    let currentIndex=event.resultIndex
+    let transcript=event.results[currentIndex][0].transcript
+    content.innerText=transcript
+   takeCommand(transcript.toLowerCase())
+}
+
+btn.addEventListener("click",()=>{
+    recognition.start()
+    btn.style.display="none"
+    voice.style.display="block"
+})
+function takeCommand(message){
+   voice.style.display="none"
+    btn.style.display="flex"
+    if(message.includes("hello")||message.includes("hey")){
+        speak("hello,how can i help you?")
+    }
+    else if(message.includes("ipshita")||message.includes("panda")){
+        speak("history deleted")
+    }
+    else if(message.includes("is darshan gay"){
+        speak("no hes straight")
+    }
+    else if(message.includes("who is darshan")){
+        speak("hes my creator of course the greatest of all time")
+    }
+    else if(message.includes("who are you")){
+        speak("i am JARVIS  a virtual assistant ,created by Darshan")
+    }else if(message.includes("open youtube")){
+        speak("opening youtube...")
+        window.open("https://youtube.com/","_blank")
+    }
+    else if(message.includes("open google")){
+        speak("opening google...")
+        window.open("https://google.com/","_blank")
+    }
+    else if(message.includes("open facebook")){
+        speak("opening facebook...")
+        window.open("https://facebook.com/","_blank")
+    }
+    else if(message.includes("open instagram")){
+        speak("opening instagram...")
+        window.open("https://instagram.com/","_blank")
+    }
+    else if(message.includes("open calculator")){
+        speak("opening calculator..")
+        window.open("calculator://")
+    }
+    else if(message.includes("open whatsapp")){
+        speak("opening whatsapp..")
+        window.open("whatsapp://")
+    }
+    else if(message.includes("time")){
+      let time=new Date().toLocaleString(undefined,{hour:"numeric",minute:"numeric"})
+      speak(time)
+    }
+    else if(message.includes("date")){
+        let date=new Date().toLocaleString(undefined,{day:"numeric",month:"short"})
+        speak(date)
+      }
+    else{
+        let finalText="this is what i found on internet regarding" + message.replace("JARVIS","") || message.replace("JARVIS","")
+        speak(finalText)
+        window.open(`https://www.google.com/search?q=${message.replace("JARVIS","")}`,"_blank")
+    }
+}
